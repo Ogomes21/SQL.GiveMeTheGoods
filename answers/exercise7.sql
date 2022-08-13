c0b2131cc4155a919ecce0b2f3212483eeb6b6b3
octavia <Ogomes21@gmail.com>
2022-08-13 12:50:46 -0400

    Finished exercise7.

---
+++ answers/exercise7.sql
@@ -0,0 +1,7 @@
+mysql> SELECT * FROM Courses WHERE CreditHours BETWEEN 10 AND 20;
++------------+-------------+
+| CourseName | CreditHours |
++------------+-------------+
+| Java101    |          11 |
+| SQL        |          10 |
++------------+-------------+
