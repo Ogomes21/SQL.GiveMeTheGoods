f871a0ecb67827b4008e10da8230017930b9d409
octavia <Ogomes21@gmail.com>
2022-08-13 12:32:46 -0400

    Finished exercise6.

--- answers/exercise5.sql
+++ answers/exercise5.sql
@@ -1,10 +1,1 @@
-mysql> SELECT * FROM Students WHERE Country NOT LIKE 'Sint Maarten' AND Country NOT LIKE 'Haiti';
-+-------------+--------------------+-----------+------------+----------+
-| StudentName | Address            | City      | PostalCode | Country  |
-+-------------+--------------------+-----------+------------+----------+
-| Jane Doe    | 57 Union St        | Edinburgh |      19701 | Scotland |
-| Octavia     | 158 Portside Court | Bear      |      19701 | USA      |
-| Fitru       | 221 Baker Street   | London    |      52013 | USA      |
-| Eugene      | 215 Tina Drive     | Langhorn  |      19047 | USA      |
-| Wilman      | 14 Avenu           | NewArk    |      98128 | USA      |
-+-------------+--------------------+-----------+------------+----------+
+mysql> SELECT * FROM Students WHERE COUNTRY IN ('Sint Maarten', 'Haiti');
